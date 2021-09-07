f416fbb4e7123cd5d7c62532be57f613c395e725
guledmoh12 <guledmo12@gmail.com>
2021-09-07 12:57:47 -0400

    Created a method that calculate player's PPG in a set season

--- Desktop/nba-stat-comparator-repo/nba-stat-comparator.py
+++ Desktop/nba-stat-comparator-repo/nba-stat-comparator.py
@@ -8,7 +8,6 @@ from matplotlib import cm
 
 #NBA stat database.
 from nba_api.stats.static import players
-from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.endpoints import playercareerstats
 
 #Makes sure that the entire dataframe is displayed.
@@ -16,18 +15,16 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-def players_stat_detail(name,id,type):
+def ppg(name):
     player_dict = [player for player in players.get_players() if player['full_name'] == name][0]
 
     #Dataframe that illustrates player's career stats.
     career = playercareerstats.PlayerCareerStats(player_id=player_dict['id'])
-    career_dataframe = career.get_data_frames()[0]
 
-    team_id = career_dataframe[career_dataframe['SEASON_ID'] == id]['TEAM_ID']
+    
+    career_array = np.array(career.get_data_frames()[0])
 
-    shotchartlist = shotchartdetail.ShotChartDetail(team_id = int(team_id), player_id = int(player_dict['id']), season_type_all_star = type, season_nullable = id, context_measure_simple = "FGA").get_data_frames()
-
-    print(career_dataframe)
+    return career_array[12][26]/career_array[12][6]
 
 
 
@@ -44,7 +41,7 @@ def draw(graphic, n1, i1, t1, n2, i2, t2):
     ax.text(1, 0.90, i2, transform=ax.transAxes, horizontalalignment='right', color='black', fontsize=9)
     ax.text(1, 0.85, t2, transform=ax.transAxes, horizontalalignment='right', color='black', fontsize=9)
     
-    ax.text(1, 0.85, t2, transform=ax.transAxes, horizontalalignment='right', color='black', fontsize=9)
+    ax.text(0, 0.5, 'Career PPG: ' + str(ppg(n1)), transform=ax.transAxes, color='black', fontsize=9)
 
     return graphic
 
@@ -52,8 +49,6 @@ if __name__ == "__main__":
 
     #Generates title.
     title = 'Test'
-
-    players_stat_detail('Kevin Durant',2020-21,'Regular Season')
 
     #Asks user for the two players' general info.
     player_name1 = input ("Enter player 1's full name: ")
