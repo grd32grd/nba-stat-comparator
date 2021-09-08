a962ae09791956362181451e65eb3bcba548a44b
guledmoh12 <guledmo12@gmail.com>
2021-09-08 14:57:58 -0400

    Create a method that'll find player codes, making it easy to import their photos into the program.

--- Desktop/nba-stat-comparator-repo/nba-stat-comparator.py
+++ Desktop/nba-stat-comparator-repo/nba-stat-comparator.py
@@ -3,8 +3,13 @@ from nba_api.stats.library.parameters import NumberOfGames
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from matplotlib.patches import Circle, Rectangle, Arc, Polygon
 import seaborn as sns
 from matplotlib import cm
+from PIL import Image
+import requests
+from io import BytesIO
 
 #NBA stat database.
 from nba_api.stats.static import players
@@ -54,6 +59,16 @@ def apg(name,year,type):
             return career_array[i][21]/career_array[i][6]
     return 0
 
+def player_codes(name,year,type):
+    player_dict = [player for player in players.get_players() if player['full_name'] == name][0]
+
+    #Multi-Dimensional Array that illustrates player's career stats.
+    career = playercareerstats.PlayerCareerStats(player_id=player_dict['id'])
+    career_array = np.array(career.get_data_frames()[0])
+
+    #Return player's code
+    return career_array[0][0]
+
 
 def draw(graphic, n1, i1, t1, n2, i2, t2):
     if graphic is None:
@@ -75,6 +90,10 @@ def draw(graphic, n1, i1, t1, n2, i2, t2):
     ax.text(1, 0.80, t2 + ' RPG: ' + "{:.2f}".format(rpg(n2,i2,t2)), horizontalalignment='right', transform=ax.transAxes, color='black', fontsize=9)
     ax.text(1, 0.75, t2 + ' APG: ' + "{:.2f}".format(apg(n2,i2,t2)), horizontalalignment='right', transform=ax.transAxes, color='black', fontsize=9)
     
+    response1 = requests.get('https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/' + str(player_codes(n1,i1,t1)) + '.png')
+    response2 = requests.get('https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/' + str(player_codes(n2,i2,t2)) + '.png')
+    img1 = Image.open(BytesIO(response1.content))
+    img2 = Image.open(BytesIO(response2.content))
 
     return graphic
 
@@ -84,24 +103,24 @@ if __name__ == "__main__":
     title = 'Test'
 
     #Asks user for the two players' general info.
-    player_name1 = input ("Enter player 1's full name: ")
-    season_id1 = input ("Input season in format (####-##): ")
-    season_type1 = input ("Regular Season (R) or Playoffs (P): ")
-    if season_type1 == 'R':
-        season_type1 = 'Regular Season'
-    elif season_type1 == 'P':
-        season_type1 = 'Playoffs'
+    # player_name1 = input ("Enter player 1's full name: ")
+    # season_id1 = input ("Input season in format (####-##): ")
+    # season_type1 = input ("Regular Season (R) or Playoffs (P): ")
+    # if season_type1 == 'R':
+    #     season_type1 = 'Regular Season'
+    # elif season_type1 == 'P':
+    #     season_type1 = 'Playoffs'
 
-    player_name2 = input ("Enter player 2's full name: ")
-    season_id2 = input ("Input season in format (####-##): ")
-    season_type2 = input ("Regular Season (R) or Playoffs (P): ")
-    if season_type2 == 'R':
-        season_type2 = 'Regular Season'
-    elif season_type2 == 'P':
-        season_type2 = 'Playoffs'    
+    # player_name2 = input ("Enter player 2's full name: ")
+    # season_id2 = input ("Input season in format (####-##): ")
+    # season_type2 = input ("Regular Season (R) or Playoffs (P): ")
+    # if season_type2 == 'R':
+    #     season_type2 = 'Regular Season'
+    # elif season_type2 == 'P':
+    #     season_type2 = 'Playoffs'    
     
     #Default, for testing.  
-    #draw(None, 'Kevin Durant','2020-21','Playoffs','James Harden','2020-21','Playoffs')
-    draw(None, player_name1, season_id1, season_type1, player_name2, season_id2, season_type2)
+    draw(None, 'Kevin Durant','2020-21','Playoffs','James Harden','2020-21','Playoffs')
+    #draw(None, player_name1, season_id1, season_type1, player_name2, season_id2, season_type2)
     plt.show()
    
